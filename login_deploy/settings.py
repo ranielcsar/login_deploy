fd4fd4cec66ffae83ff3072d81d26130346ef804
ranoob <ranielcsardev@gmail.com>
2024-11-11 17:32:11 -0300

    fix: hosts permitidos

--- login_deploy/settings.py
+++ login_deploy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t6m)h+dx!bdlj!rqgyestavhyjcpc*upi@74qcajxsfp^sajik"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "0.0.0.0"]
 
 
 # Application definition
